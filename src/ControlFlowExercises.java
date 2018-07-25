5fe49247ad370eef8b067b16db8a43104a56dab7
Robin Maxwell <robinmaxwell@Robin-Maxwells-MacBook-Pro.local>
2018-07-25 11:02:29 -0500

    Control Flow Exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+        }
+        int doNum = 0;
+        do {
+            System.out.println(doNum);
+        } while (doNum % 2 == 0 && doNum <= 100);
+            doNum++;
+
+        int backNum = 100;
+        do {
+            System.out.println(backNum);
+        } while (backNum >= -10);
+            backNum -= 5;
+
+        int squareNum = 0;
+        do {
+            System.out.println(squareNum * squareNum);
+        } while (squareNum < 1000000);
+
+        fizzNum = 1;
+        while (fizzNum <= 100 && fizzNum % 3 == 0) {
+            System.out.println(fizzNum);
+        }
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What number would you like to go up to?");
+        userInput1 = sc.next();
+
+        System.out.print("Here is your table!"
+                for (i = 1; i <= userInput1; i += 1);
+                    System.out.println(
+                \n "Number |  Square | Cubed" \n
+                "------  | ------  |  ------
+                "" + (userInput1) + "        " +   "|" +     (userInput1 * userInput1) +    "|"  +  (userInput1 * userInput1 * userInput1));
+    }
+
+
+
+//Solution/*
+
+int i = 5;
+while (i<= 15) {
+    System.out.print(i);
+    i ++;
+}
+
+//-------------------//
+
+int i = 0;
+do {
+    System.out.println(i);
+    i += 2;
+    //i -= 5;
+    } while (i <= 100);
+    //while (i >= -10);
+
+        //---------------//
+
+//int i = 2;
+long i =2;
+do {
+    System.out.println(i);
+    i *= i;
+        } while (i < 1000000);
+
+//-----------------//
+
+
+//FizzBuzz//
+
+for(int i = 0; i <= 100; i+=1)
+
+    {
+        if (i % 15 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (i % 5 == 0) {
+            System.out.println("Buzz");
+        } else if (i % 3 == 0) {
+            System.out.println("Fizz");
+        } else {
+            System.out.println(i);
+        }
+    }
+
+
+}
+
