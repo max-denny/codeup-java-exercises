7e0ac1c1ed1b267c93c9e6472d85c2fe896f5bf2
Robin Maxwell <robinmaxwell@Robin-Maxwells-MacBook-Pro.local>
2018-08-01 09:27:42 -0500

    Commit Movie Lister APP

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,79 @@
+package movies;
+import util.Input;
+
+public class MoviesApplication {
+
+
+    public static void main(String[] args) {
+
+        Input input = new Input();
+
+
+        System.out.println("What would you like to do? : \n \n" +
+                "0 - exit \n" +
+                "1 - view all movies \n" +
+                "2 - view movies in the animated category \n" +
+                "3 - view movies in the drama category \n" +
+                "4 - view movies in the horror category \n" +
+                "5 - view movies in the scifi category \n");
+
+        int i;
+
+        do {
+            System.out.println("Enter your choice: ");
+
+            i = input.getInt();
+
+            if (i == 1){
+
+                for(Movie movie: MoviesArray.findAll()) {
+                        System.out.println(movie.getName() + " " +  movie.getCategory());
+                    }
+                } else if (i == 2) {
+
+                    for(Movie movie: MoviesArray.findAll()) {
+
+                        if (movie.getCategory().equals("animated")) {
+
+                            System.out.println(movie.getName() + " " + movie.getCategory());
+                        }
+                    }
+
+            } else if (i == 3) {
+
+                for(Movie movie: MoviesArray.findAll()) {
+
+                    if (movie.getCategory().equals("drama")) {
+
+                        System.out.println(movie.getName() + " " +  movie.getCategory());
+
+                    }
+                }
+
+            } else if (i == 4) {
+
+                for(Movie movie: MoviesArray.findAll()) {
+
+                    if (movie.getCategory().equals("horror")) {
+
+                        System.out.println(movie.getName() + " " + movie.getCategory());
+
+                    }
+                }
+
+            } else if (i ==5) {
+
+                for(Movie movie: MoviesArray.findAll()) {
+
+                    if (movie.getCategory().equals("scifi")) {
+
+                        System.out.println(movie.getName() + " " + movie.getCategory());
+
+                    }
+                }
+            }
+
+        } while (i != 0);
+    }
+}
+
