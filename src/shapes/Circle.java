82d404f1a47e31cf9c7e027d0762f8da7bffa206
Robin Maxwell <robinmaxwell@Robin-Maxwells-MacBook-Pro.local>
2018-08-01 16:33:44 -0500

    Shapes-- Inheritance && Polymorphism

---
+++ src/shapes/Circle.java
@@ -0,0 +1,28 @@
+package shapes;
+
+public class Circle {
+    private static int circleCount = 0;
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        circleCount++;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getCircumference() {
+        return Math.PI * 2 * radius;
+    }
+
+    public static int getCircleCount() {
+        return circleCount;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
+
+
+
