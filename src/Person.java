13546185a4b1033b3df1e5e4504ed32ff26f0969
MRDenny <mrdenny@MRDennys-MacBook-Pro.local>
2018-08-03 16:46:39 -0500

    Commit Java Refactor

--- src/Person.java
+++ src/Person.java
@@ -1,72 +1,14 @@
 public class Person {
-/*
+
     private String name;
 
-    public Person(){}
-
-    public Person(String personName) {
+    public Person(String personName){
         name = personName;
     }
 
-    public String getName(){
-        return name;
+    public Person(){
 
     }
-
-    public void setName(String name){
-        this.name = name;
-
-
-    }
-
-    public void sayHello(){
-        System.out.println("Hello" + " " + name);
-
-    }
-
-    public static void main(String[] args){
-
-        Person person = new Person();
-
-        /*Person person1 = new Person("John");
-        Person person2 = new Person("John");
-        System.out.println(person1.getName().equals(person2.getName()));
-        System.out.println(person1 == person2);
-*/
-
-      /*  Person person1 = new Person("John");
-        Person person2 = person1;
-        System.out.println(person1 == person2);
-*/
-     /* Person person1 = new Person("John");
-      Person person2 = person1;
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
-        person2.setName("Jane");
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
-*/
-
-/*
-
-        System.out.println(person.getName());
-        person.setName("Jim");
-        System.out.println(person.getName());
-        person.sayHello();
-
-        //Person person2 = new Person("Ted");
-
-        //System.out.println(person2.getName());
-        System.out.println(person.getName());
-    }
-
-*/
-
-//Solution//
-
-/*public class Person {
-
-    private String name;
 
     public String getName(){
         return this.name;
@@ -74,26 +16,36 @@ public class Person {
 
     public void setName(String name){
         this.name = name;
+
     }
 
     public void sayHello(){
-        System.out.println("Hello" + " " + this.name);
+        System.out.println("Hello " + name);
     }
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Person person = new Person();
-        person.setName("Gabrielle");
+        person.setName("Max");
         person.sayHello();
 
-        Person employee = new Person();
-        employee.setName("Jennifer");
-        employee.sayHello();
+        Person person1 = new Person("John");  //Define person1 as a new object with the name of John//
+        Person person2 = new Person("John"); //Define person2 as a new object with the name of John//
+        System.out.println(person1.getName().equals(person2.getName())); //Print: name 'property' of person1 is equal to name 'property' of person2//
+        System.out.println(person1 == person2);//Print person 1 is equal to person 2//
 
+        /*Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1 == person2);*/
 
+        /*Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());*/
 
 
     }
-*/}
 
-
+}
