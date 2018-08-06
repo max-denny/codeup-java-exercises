4fcc15710c7801c38ab2ac1bc2e38e820523c0fa
MRDenny <mrdenny@MRDennys-MacBook-Pro.local>
2018-08-05 23:35:46 -0500

    Add refactor of /Input/ and /Input Test/

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,27 +1,105 @@
+
+//THIS ENTIRE CLASS IS ABOUT RECEIVING INPUT FROM THE USER//
+
 package util;
 
 import java.util.Scanner;
 
 public class Input {
-/*
-    private Scanner sc = new Scanner(System.in);
+    //This line creates new scanner(from Java class library) that we name 'scan'.
+    private Scanner scan = new Scanner(System.in);
 
 
-    //Get input from user as string//
+    //The first thing we want to do is get a string from the user.//
 
-    public String getString(String prompt){
-        prompt = (prompt.isEmpty()) ? "Type: ": prompt;
+
+    //This line will initialize our method//
+    public String getString(String prompt) {
+        //This line will print out the prompt we provide in the parameter for the user//
         System.out.println(prompt);
-        return this.sc.nextLine().trim();
-
+        //This line returns what the string the user provided in lower case w/o outside white space//
+        return this.scan.nextLine().toLowerCase().trim();
     }
 
-    public boolean yesNo(){
+
+
+    //RETURNS TRUE IF THE USER ENTERS YES OR EQUIVALENT//
+
+    //This line defines a method we name 'yesNo'//
+    public boolean yesNo() {
+        //This line://
+        // defines a property of the method we assign as 'result'//
+        // assigns it to whatever the user inputs via 'getString' method//
+        //With 'Type yes or no' prompt'//
         String result = this.getString("Type yes or no (y/n)");
+        //This line returns a true value if the user enters 'yes' or 'y' equivalent//
         return (result.equals("y") || result.equals("yes"));
+    }
+
+    //---------------//
+
+
+    //GET AN INTEGER VALUE WITHIN RANGE FROM USER//
+
+    public int getInt(int min, int max) {
+
+        int userInput = Integer.parseInt(this.getString(
+                "Give me a number between " + min +
+                        " and " + max));
+
+        if (userInput < min || userInput > max) {
+            System.err.println("Number out of range");
+            getInt(min,max);
+        }
+
+        return userInput;
+    }
+
+    //------------------------------//
+
+
+    // GET AN INTEGER VALUE FROM THE USER//
+    public int getInt(){
+        return Integer.parseInt(this.getString("Give me a number"));
 
     }
-*/
+
+
+    // GET A DOUBLE VALUE FROM THE USER//
+
+    public double getDouble(double min, double max){
+        double userInput = Double.parseDouble(this.getString("Give me a a decimal" +
+                "number between " + min + " and " + max));
+
+        if(userInput < min || userInput > max){
+            System.err.println("Decimal number out of range");
+            userInput = getDouble(min,max);
+        }
+
+        return userInput;
+    }
+
+    public double getDouble(){
+        return Double.parseDouble(this.getString("Give me a decimal number"));
+    }
+
+
+    public static void main(String[] args) {
+        Input input1 = new Input();
+
+
+        System.out.println(input1.getString("Type your name in any format: "));
+
+        System.out.println(input1.yesNo());
+
+        System.out.println(input1.getInt(5, 6));
+
+        System.out.println(input1.getDouble(2, 20));
+
+        System.out.println(input1.getDouble());
+
+    }
+
 }
 
-
+//---------------------------------------------------------------------------------//
